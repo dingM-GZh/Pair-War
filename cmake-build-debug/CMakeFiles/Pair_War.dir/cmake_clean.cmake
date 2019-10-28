199295598ab5f5d597980ef4984b05f2924e1da4
Maxwell Ding <mbding@gmail.com>
2019-10-28 01:56:51 -0500

    First upload. Included pthread library. That's it

---
+++ cmake-build-debug/CMakeFiles/Pair_War.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Pair_War.dir/main.cpp.obj"
+  "Pair_War.exe"
+  "Pair_War.exe.manifest"
+  "Pair_War.pdb"
+  "libPair_War.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Pair_War.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include <pthread.h>
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
