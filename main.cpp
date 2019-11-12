cb2ff3368f444ea7e88b0e55b71734c468432dcf
Maxwell Ding <mbding@gmail.com>
2019-11-12 17:56:28 -0600

    Working with threading the players. Need to do rotation until pair is found. Also need to get players to signal correctly

--- main.cpp
+++ main.cpp
@@ -6,6 +6,7 @@
 #include <fstream>
 #include <stdlib.h>
 #include <deque>
+#include <cstdint>
 #include "Player.h"
 
 using namespace std;
@@ -18,7 +19,7 @@ using namespace std;
 #define ACE 14
 
 void *dealer_moves(void *);
-void *player_moves( void *);
+void *player_moves(void *);
 void *square_moves(void *);
 
 void init();
@@ -65,7 +66,7 @@ deque <int> deck;
 stack <int> shuffle;
 
 ofstream fout;
-int current_player, current_round = 1;
+int current_player = 0, current_round = 0, hot_potato = 0;
 bool pair_found = false;
 
 int main() {
@@ -73,9 +74,9 @@ int main() {
 
     pthread_create(&dealer_thread, NULL, dealer_moves, NULL);
     //_sleep(1);
-    pthread_create(&players_thread[0], NULL, player_moves, (void *) 1);
-    //pthread_create(&players_thread[1], NULL, player_moves, (void *) 2);
-    //pthread_create(&players_thread[2], NULL, player_moves, (void *) 3);
+    pthread_create(&players_thread[0], NULL, player_moves, (void *) 0);
+    pthread_create(&players_thread[1], NULL, player_moves, (void *) 1);
+    pthread_create(&players_thread[2], NULL, player_moves, (void *) 2);
 
     //pthread_create(&player1_thread, NULL, player_moves, (void *)1);
     //pthread_create(&player2_thread, NULL, player_moves, (void *)2);
@@ -83,8 +84,8 @@ int main() {
 
     pthread_join(dealer_thread, NULL);
     pthread_join(players_thread[0],NULL);
-    //pthread_join(players_thread[1],NULL);
-    //pthread_join(players_thread[2],NULL);
+    pthread_join(players_thread[1],NULL);
+    pthread_join(players_thread[2],NULL);
     //pthread_join(player1_thread, NULL);
     //pthread_join(player2_thread, NULL);
     //pthread_join(player3_thread, NULL);
@@ -208,7 +209,7 @@ void *dealer_moves(void *) {
     //deal_process(player3);
 
     //pthread_cond_wait(&dealer_cond, &dealer_mutex);
-    pthread_cond_signal(&players_cond[current_round - 1]);
+    pthread_cond_signal(&players_cond[current_round-1]);
     pthread_cond_wait(&dealer_cond, &dealer_mutex);
     cout << "ending dealer moves" << endl;
     pthread_mutex_unlock(&dealer_mutex);
@@ -219,23 +220,38 @@ void *dealer_moves(void *) {
 
 void *player_moves(void *player_id) {
     pthread_mutex_lock(&players_mutex);
-    current_player = current_round;
-    pthread_cond_wait(&players_cond[current_player - 1], &players_mutex);
-    int temp = deck.front();
-    deck.pop_front();
-    cout << "temp " << temp << endl;
+    pthread_cond_wait(&players_cond[*((int*)(&player_id))-1], &players_mutex);
 
+    int temp = deck.front(); // the second that is to be drawn
+    deck.pop_front(); // deletes new card from top of deck
+    //cout << "temp " << temp << endl;
+    int id = *((int*)(&player_id));
+    Player thread_player = players[id]; // thread specific player;
 
-    if (current_round == 1) {
-        pair_found = compare_cards(players[current_player-1], temp);
-        cout << pair_found << endl;
+    if (current_round == id) {
+        cout << thread_player.get_name() << ": draws " << temp << endl;
+        fout << thread_player.get_name() << ": draws " << temp << endl;
+        pair_found = compare_cards(thread_player, temp);
+        //cout << pair_found << endl;
+        if (pair_found == false) {
+            cout << thread_player.get_name() << ": discard " << temp << endl;
+            fout << thread_player.get_name() << ": discard " << temp << endl;
+            deck.push_back(temp);
+            display_deck();
+
+            pthread_cond_signal(&players_cond[id]);
+        }
+        else {
+            // stuff to terminate the program bc pair is found
+        }
+        //cout << "player 1" << endl;
     }
     else {
-        //
+        cout << "not player 1" << endl;
     }
 
-    deck.push_back(temp);
-    temp = deck.front();
+    //deck.push_back(temp);
+    //temp = deck.front();
 
     /***
     if (*(int *) player_id == 1) {
@@ -258,12 +274,12 @@ void *player_moves(void *player_id) {
     } */
 
     pthread_cond_signal(&dealer_cond);
-    cout << "ending player moves" << endl;
+    cout << "ending player moves "<< endl;
     pthread_mutex_unlock(&players_mutex);
     return NULL;
 }
 
-void *square_moves(void *) {
+void *square_moves(void *) { // "fun" easter egg
     cout << "Square moves?" << endl << endl;
     exit(1);
 }
@@ -272,25 +288,22 @@ void *square_moves(void *) {
 void deal_process(Player &player) {
     int top_card = deck.front();
 
-    string name = player.get_name();
-    player.set_card(top_card);
+    string name = player.get_name(); // easy access to player name
+    player.set_card(top_card); // assign the main card (doesn't change)
 
     //cout << "DEALER: " << name << " is dealt " << top_card << endl << endl;
     //fout << "DEALER: " << name << " is dealt " << top_card << endl << endl;
 
-    cout << name << ": hand "  << player.get_card() << endl;
+    cout << name << ": hand "  << player.get_card() << endl; // delete when complete
     fout << name << ": hand "  << top_card << endl;
 
-
-    deck.pop_front();
-
-    cout << deck.size() << endl;
-
-    display_deck();
-    //deck.pop_back(); // takes top card off stack
+    deck.pop_front(); // deletes card from top of deck
+    cout << deck.size() << endl; // delete when complete
+    display_deck(); // delete when complete
 }
 
 bool compare_cards(Player player, int temp) {
-    cout << "card " << player.get_card() << "\t temp " << temp << endl;
-    return player.get_card() == temp;
+    cout << "compare_cards" << endl
+    << "card " << player.get_card() << "\t temp " << temp << endl;
+    return (player.get_card() == temp);
 }
